9fc4a1583b862ddd9b46cb7b881508b753e63f85
Hannah Cline <hannahcline@Hannahs-MacBook-Air.local>
2020-02-20 11:21:55 -0500

    fix so both tags and product types can be fetched

--- src/containers/filters.js
+++ src/containers/filters.js
@@ -33,7 +33,7 @@ class Filters extends Component {
       tags:
       ['gluten+free', 'cruelty+free'],
 
-      productTypeClicked:[]}
+      productTypeClicked:''}
 
     //this.onInputChange = this.onInputChange.bind(this);
     this.onFormSubmit = this.onFormSubmit.bind(this);
@@ -48,8 +48,9 @@ class Filters extends Component {
     event.preventDefault();
     
     // fetch make-up data
+    this.state.productTypeClicked = event.target.id;
     for (let i = 0; i < this.state.tags.length; i++){
-      this.props.fetchMakeUp(this.state.tags[i])
+      this.props.fetchMakeUp(this.state.tags[i], this.state.productTypeClicked)
     }
     
 
@@ -57,10 +58,8 @@ class Filters extends Component {
     console.log('event inside onFormSubmit ', event.target.id)  
     
 
-    // fetch make-up data
-    this.state.productTypeClicked = event.target.id;
     
-    this.props.fetchMakeUp(this.state.productTypeClicked);
+  
     console.log('data ', this.state)
     //event.preventDefault();
 
